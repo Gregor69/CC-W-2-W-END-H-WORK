268bbd731e2f2943ea9caa030fd9595fc9a2c2cd
Gregor Simpson <catkelly@Catrionas-MacBook-Pro.local>
2021-01-17 12:56:37 +0000

    updated homework

--- src/room.py
+++ src/room.py
@@ -1,3 +1,25 @@
 class Room:
     def __init__(self, name):
         self.name = name
+        self.guests = []
+        self.songs = []
+
+    def guest_count(self):
+        return len(self.guests)
+
+    def checkin_guest(self, guest):
+        self.guests.append(guest)
+
+    def checkout_guest(self, guest):
+        self.guests.remove(guest)
+
+    def song_count(self):
+        return len(self.songs)
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+    def remove_song(self, song):
+        self.songs.remove(song)
+
+    
--- tests/room_test.py
+++ tests/room_test.py
@@ -7,8 +7,27 @@ from src.song import Song
 class TestRoom(unittest.TestCase):
 
     def setUp(self):
-        self.room = Room("Give us a tune, son")
-        # self.song= Song("Dry the Rain", "The Beta Band")
+        self.room = Room("Give us a tune")
+        self.guest_1 = Guest("Alfie")
+        self.song_1 = Song("Dry the Rain", "The Beta Band")
 
     def test_room_has_a_name(self):
-        self.assertEqual("Give us a tune, son", self.room.name)
+        self.assertEqual("Give us a tune", self.room.name)
+
+    def test_room_can_checkin_guest(self):
+        self.room.checkin_guest(self.guest_1)
+        self.assertEqual(1, self.room.guest_count())
+
+    def test_room_can_checkout_guest(self):
+        self.room.checkin_guest(self.guest_1)
+        self.room.checkout_guest(self.guest_1)
+        self.assertEqual(0, self.room.guest_count())
+
+    def test_room_can_add_song(self):
+        self.room.add_song(self.song_1)
+        self.assertEqual(1, self.room.song_count())
+
+    def test_room_can_remove_song(self):
+        self.room.add_song(self.song_1)
+        self.room.remove_song(self.song_1)
+        self.assertEqual(0, self.room.song_count())
